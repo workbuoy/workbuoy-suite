584c11f878332749c4af1706b5a44ef7307c82d6
workbuoy <workbuoy@outlook.com>
2025-09-03 15:03:26 +0100

    backend update

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,9 +1,14 @@
-export default {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec|test).ts'],
-  moduleFileExtensions: ['ts', 'js'],
-  roots: ['<rootDir>/tests', '<rootDir>/src'],
-  transform: { '^.+\\.ts$': ['ts-jest', { isolatedModules: true }] },
-  verbose: false
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { isolatedModules: true }],
+  },
+  moduleNameMapper: {
+    // Resolve TS files when imports end with .js (e.g., './rbac/policies.js')
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
