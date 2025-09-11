9200e9a3cc0b1c721786d9ff1a7c1045c219e8c1
workbuoy <workbuoy@outlook.com>
2025-09-11 22:36:59 +0100

    Add files via upload

---
+++ frontend/src/main.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App(){ return <div>Workbuoy Navi</div> }
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
