d1c62f906d3193a144eab53355998eefa568b0ea
workbuoy <workbuoy@outlook.com>
2025-09-25 21:20:18 +0200

    chore(workspaces): expand npm workspaces to include satellites

---
+++ docs/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: false,
+  extends: ['../.eslintrc.cjs'],
+  env: {
+    jest: true,
+  },
+  ignorePatterns: ['**/*.md'],
+};
---
+++ examples/js/.eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  root: false,
+  extends: ['../../.eslintrc.cjs'],
+  env: {
+    jest: true,
+  },
+};
