b36fe287b8a1605fb73ef477b3958b7d7c8bd502
workbuoy <workbuoy@outlook.com>
2025-09-27 22:07:13 +0200

    chore: migrate eslint to flat config (#256)

---
+++ tools/empty-parser.mjs
@@ -0,0 +1,14 @@
+export function parse() {
+  return {
+    type: 'Program',
+    body: [],
+    sourceType: 'module',
+    tokens: [],
+    comments: [],
+    loc: {
+      start: { line: 1, column: 0 },
+      end: { line: 1, column: 0 },
+    },
+    range: [0, 0],
+  };
+}
