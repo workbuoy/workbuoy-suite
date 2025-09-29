23da79583a139f62f06c3c4125997099a99d6e03
workbuoy <workbuoy@outlook.com>
2025-09-29 16:42:35 +0200

    fix(ui/storybook): align vite builder dependencies

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,10 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  framework: { name: '@storybook/react-vite', options: {} },
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
-  core: { disableTelemetry: true },
   docs: { autodocs: 'tag' },
 };
 
